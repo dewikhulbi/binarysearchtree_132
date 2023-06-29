6e829bf14459378b21b2e7fc32681d667f477a8d
dewikhulbi <dewikhlb20@gmail.com>
2023-06-29 19:13:10 +0700

    membuat pengulangan while

--- binarysearchtree_132/binarysearchtree_132.cpp
+++ binarysearchtree_132/binarysearchtree_132.cpp
@@ -54,8 +54,14 @@ public:
 		}
 	}
 
-	void search(string element, Node*& parent, Node*& currentNOde)
+	void search(string element, Node*& parent, Node*& currentNode)
 	{
+		// this function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
 
+		}
 	}
 };
