5c70bd8c993470b30c1fc0b67be9cf240648bc28
dewikhulbi <dewikhlb20@gmail.com>
2023-06-29 19:27:58 +0700

    membuat tampilan menu pilihan di layar

--- binarysearchtree_132/binarysearchtree_132.cpp
+++ binarysearchtree_132/binarysearchtree_132.cpp
@@ -120,4 +120,13 @@ int main()
 {
 	BinaryTree obj;
 	while (true)
+	{
+		cout << "\nMenu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. Perform preorder traversal" << endl;
+		cout << "4. Perform postrder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5): " << endl;
+	}
 }
